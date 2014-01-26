84e6b94e61e166b7826b9e5679bf7151265d579d
David Selassie <selassid@gmail.com>
2014-01-26 09:01:43 -0800

    Gives foxes hella pitchforks.

--- Assets/Standard Assets/Scripts/AttackComponent.js
+++ Assets/Standard Assets/Scripts/AttackComponent.js
@@ -3,13 +3,18 @@
 public var attackStrength : float = 5.0f;
 public var attackRadius : float = 1.25f;
 public var attackCycleSeconds : float = 0.75f;
+public var attackTool : GameObject;
 
 private var lastAttackTimeSeconds : float = Mathf.NegativeInfinity;
+private var motorComponent : CharacterMotor;
+private var toolAnimator : Animator;
+
+function Awake () {
+	motorComponent = GetComponent(CharacterMotor);
+	toolAnimator = attackTool.GetComponent(Animator);
+}
 
 function Update () {
-	if (Input.GetButtonDown("Fire1")) {
-		TryAttack();
-	}
 }
 
 public function TryAttack () {
@@ -23,15 +28,24 @@ private function CanAttackNow () : boolean {
 }
 
 private function Attack () {
-	var nearbyColliders : Collider[] = Physics.OverlapSphere(transform.position, attackRadius);
-	for (var collider : Collider in nearbyColliders) {
+	var hits : RaycastHit[] = Physics.RaycastAll(gameObject.transform.position, motorComponent.lastInputMoveDirection, attackRadius);
+	for (var hit : RaycastHit in hits) {
 		// Don't hurt anyone with the same type tag.
-		if (collider.gameObject.tag != gameObject.tag) {
-			var livingComponent : LivingComponent = collider.gameObject.GetComponent(LivingComponent);
+		if (hit.collider.gameObject.tag != transform.parent.gameObject.tag) {
+			var livingComponent : LivingComponent = hit.collider.gameObject.GetComponent(LivingComponent);
 			if (livingComponent) {
 				livingComponent.Hurt(attackStrength);
 			}
 		}
 	}
 	lastAttackTimeSeconds = Time.time;
+
+	// Use your muscles.
+	toolAnimator.SetInteger("State", 2);
+	StartCoroutine(ResetAnimationStateSoon());
+}
+
+private function ResetAnimationStateSoon () : IEnumerator {
+	yield WaitForSeconds(attackCycleSeconds);
+	toolAnimator.SetInteger("State", 1);
 }
---
+++ Assets/Standard Assets/Scripts/PitchforkComponent.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+private var motor : CharacterMotor;
+
+function Awake () {
+	motor = transform.parent.gameObject.GetComponent(CharacterMotor);
+}
+
+function FixedUpdate () {
+	transform.localPosition = Vector3.ClampMagnitude(motor.lastInputMoveDirection, 0.3f);
+}
