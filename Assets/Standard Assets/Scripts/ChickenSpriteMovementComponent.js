7c0d1ff28bcd9308379ce832d4123f0ec76a26cb
David Selassie <selassid@gmail.com>
2014-01-25 22:33:53 -0800

    Adds Chicken sprites!

---
+++ Assets/Standard Assets/Scripts/ChickenSpriteMovementComponent.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+@script RequireComponent (CharacterMotor)
+@script RequireComponent (Animator)
+@script RequireComponent (LivingComponent)
+
+private var motor : CharacterMotor;
+private var animator : Animator;
+private var livingComponent : LivingComponent;
+
+function Awake () {
+	motor = GetComponent(CharacterMotor);
+	animator = GetComponent(Animator);
+	livingComponent = GetComponent(LivingComponent);
+}
+
+function Update () {
+	var currentState : int = animator.GetInteger("State");
+	var newState : int;
+	
+	if (!livingComponent.Alive()) {
+		newState = 5;
+	}
+	else if (motor.inputMoveDirection.sqrMagnitude < 0.01f) {
+		newState = 1;
+	}
+	else {
+		newState = 3;
+	}
+	
+	if (newState != currentState) {
+		animator.SetInteger("State", newState);
+	}
+}
