92f0c1a0c7a0d859e93ada0f26c009c38fabc45c
David Selassie <selassid@gmail.com>
2014-01-26 09:17:56 -0800

    Attacks work again but I have no idea about animation.

--- Assets/Standard Assets/Scripts/AttackComponent.js
+++ Assets/Standard Assets/Scripts/AttackComponent.js
@@ -3,18 +3,14 @@
 public var attackStrength : float = 5.0f;
 public var attackRadius : float = 1.25f;
 public var attackCycleSeconds : float = 0.75f;
-public var attackTool : GameObject;
 
 private var lastAttackTimeSeconds : float = Mathf.NegativeInfinity;
 private var motorComponent : CharacterMotor;
-private var toolAnimator : Animator;
+private var animator : Animator;
 
 function Awake () {
 	motorComponent = GetComponent(CharacterMotor);
-	toolAnimator = attackTool.GetComponent(Animator);
-}
-
-function Update () {
+	animator = GetComponent(Animator);
 }
 
 public function TryAttack () {
@@ -31,7 +27,7 @@ private function Attack () {
 	var hits : RaycastHit[] = Physics.RaycastAll(gameObject.transform.position, motorComponent.lastInputMoveDirection, attackRadius);
 	for (var hit : RaycastHit in hits) {
 		// Don't hurt anyone with the same type tag.
-		if (hit.collider.gameObject.tag != transform.parent.gameObject.tag) {
+		if (hit.collider.gameObject.tag != gameObject.tag) {
 			var livingComponent : LivingComponent = hit.collider.gameObject.GetComponent(LivingComponent);
 			if (livingComponent) {
 				livingComponent.Hurt(attackStrength);
@@ -41,11 +37,5 @@ private function Attack () {
 	lastAttackTimeSeconds = Time.time;
 
 	// Use your muscles.
-	toolAnimator.SetInteger("State", 2);
-	StartCoroutine(ResetAnimationStateSoon());
-}
-
-private function ResetAnimationStateSoon () : IEnumerator {
-	yield WaitForSeconds(attackCycleSeconds);
-	toolAnimator.SetInteger("State", 1);
+	//gameObject.BroadcastMessage("AttackingNow");
 }
