6e2d5ff8be13a319001d11eaee6431cf2ecda0d8
David Selassie <selassid@gmail.com>
2014-01-26 10:19:51 -0800

    Adds in all the forgotten fox sounds.

---
+++ Assets/Standard Assets/Scripts/FoxNoiseComponent.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+public var noises : AudioClip[];
+
+function Start () {
+	StartCoroutine(MakeNoiseAndWaitLoop());
+}
+
+private function MakeNoiseAndWaitLoop () : IEnumerator {
+	var index : int = Random.Range(0.0f, noises.Length);
+	audio.PlayOneShot(noises[index]);
+	yield WaitForSeconds(Random.Range(1.0f, 4.0f));
+	StartCoroutine(MakeNoiseAndWaitLoop());
+}
